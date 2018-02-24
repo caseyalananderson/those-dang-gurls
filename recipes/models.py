4bf3fdab322516a2e8b6e9f18ba848c4363499f1
caseyalananderson <casey.alan.anderson@gmail.com>
2018-02-24 12:54:28 -0700

    all of this stuff seemed to make recipes viewable at least

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import os
+from django.conf import settings
+
+
+def get_upload_path(instance, filename):
+    """
+    Gets path of
+    :param instance:
+    :param filename:
+    :return:
+    """
+    no_space_title = str(instance.title).replace(' ', '')
+    return os.path.join("photos",
+                        no_space_title, filename)
 
 
 class Recipe(models.Model):
@@ -17,6 +31,7 @@ class Recipe(models.Model):
     description = models.TextField(null=True)
     vegan = models.BooleanField(default=False)
     health = models.CharField(choices=HEALTH_LEVEL, max_length=25)
+    photo = models.ImageField(upload_to=get_upload_path, null=True)
 
     def __str__(self):
         return self.title
