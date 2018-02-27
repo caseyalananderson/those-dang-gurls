420aa5ad210dce4e680eed4e5c59725537b4a9b0
Casey Alan Anderson <casey.alan.anderson@gmail.com>
2018-02-26 19:41:54 -0700

    updated migrations for recipes, included images and removed old ones

--- recipes/models.py
+++ recipes/models.py
@@ -10,8 +10,8 @@ def get_upload_path(instance, filename):
     :param filename:
     :return:
     """
-    no_space_title = str(instance.title).replace(' ', '')
-    return os.path.join("photos", no_space_title, filename)
+    no_space_title = str(instance.recipe.title.replace(' ', ''))
+    return os.path.join("photos", no_space_title, 'images', filename)
 
 
 class Recipe(models.Model):
@@ -28,32 +28,26 @@ class Recipe(models.Model):
 
     title = models.CharField(max_length=50)
     description = models.TextField(null=True)
+    instructions = models.TextField(null=True)
+
     vegan = models.BooleanField(default=False)
     health = models.CharField(choices=HEALTH_LEVEL, max_length=25)
-    main_photo = models.ImageField(upload_to=get_upload_path, null=True)
-    img1 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img2 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img3 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img4 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img5 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img6 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img7 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img8 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img9 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img10 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img11 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img12 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img13 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img14 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img15 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img16 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img17 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img18 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img19 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
-    img20 = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ('title',)
+
+
+class Image(models.Model):
+    """
+    Images for the Recipe
+    """
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=get_upload_path)
+    main_photo = models.BooleanField(default=False)
+    recipe = models.ForeignKey(Recipe, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
