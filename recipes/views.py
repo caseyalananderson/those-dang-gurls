8af8e4d370d28fbc725c84b269459dafb5f1f69f
Casey Alan Anderson <casey.alan.anderson@gmail.com>
2018-02-27 20:28:39 -0700

    commiting whats here, I will need to update my computer and use postgres for django to get search functionality

--- recipes/views.py
+++ recipes/views.py
@@ -41,25 +41,3 @@ def recipe_detail(request, pk):
 
     return render(request, 'recipes/recipe_detail.html', context)
 
-
-class BlogSearchListView(ListView):
-    """
-    Display a Blog List page filtered by the search query.
-    """
-
-    model = Recipe
-    paginate_by = 10
-
-    def get_queryset(self):
-        qs = Recipe.objects.all()
-
-        keywords = self.request.GET.get('q')
-        if keywords:
-            query = SearchQuery(keywords)
-            title_vector = SearchVector('title', weight='A')
-            content_vector = SearchVector('content', weight='B')
-            vectors = title_vector + content_vector
-            qs = qs.annotate(search=vectors).filter(search=query)
-            qs = qs.annotate(rank=SearchRank(vectors, query)).order_by('-rank')
-
-        return qs
--- search/views.py
+++ search/views.py
@@ -2,5 +2,40 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from recipes.models import Recipe
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.views.generic import ListView
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def search(request):
+    """
+    Searches
+    :param request:
+    :return:
+    """
+
+    qs = Recipe.objects.all()
+
+    keywords = request.GET.get('search_box')
+    this_str = "<p> %s </p>" % keywords
+    if keywords:
+        query = SearchQuery(keywords)
+        title_vector = SearchVector('title', weight='A')
+        content_vector = SearchVector('content', weight='B')
+        vectors = title_vector + content_vector
+        qs = qs.annotate(search=vectors).filter(search=query)
+        qs = qs.annotate(rank=SearchRank(vectors, query)).order_by('-rank')
+
+    #CAA TODO: SWTICH TO POSTGRES, NOT SQLLITE!!!
+
+    context = {
+        'qs': qs,
+    }
+
+    print(context)
+
+    return HttpResponse(this_str)
+    # return render(request, 'search/search_list.html', context)
