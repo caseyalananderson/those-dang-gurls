797afe49ffa2d957dd801ce1a293f25aa3ee966a
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-01 18:56:19 -0700

    removed recipes and blog

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.blog_post, name='blog_post')
-]
--- recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail')
-]
