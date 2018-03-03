6e3948c2e360d8b1825575d5baf461261feed0e3
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-03 16:55:18 -0700

    updated comment form look

--- comments/forms.py
+++ comments/forms.py
@@ -9,7 +9,7 @@ class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
     # parent_id = forms.IntegerField(widget=forms.HiddenInput)
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(label='', widget=forms.Textarea)
 
 
 
