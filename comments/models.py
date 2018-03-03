9df3f20bfc804094832b7eb4eae157e06c7618e1
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-03 13:09:23 -0700

    updated food and comment migrations in database

--- comments/models.py
+++ comments/models.py
@@ -2,5 +2,48 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.admin import GenericForeignKey
 
 # Create your models here.
+
+
+class CommentManager(models.Manager):
+    """
+    Manage for the Comment section
+    """
+    def filter_by_instance(self, instance):
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        obj_id = instance.id
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        return qs
+
+
+class Comment(models.Model):
+    """
+    Comments for the Food Entry / Recipe post
+    """
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    approved_comment = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', null=True, blank=True)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def children(self):
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent:
+            return True
+        return False
+
+    def __str__(self):
+        return self.content
