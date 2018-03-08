0f6253e4795fa6aefacce88c7dba0700f3595d57
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-07 23:50:57 -0700

    added functionality to extract the last and most recent 5 entries to the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -3,20 +3,40 @@ from food.models import FoodPost
 from fitness.models import FitnessPost
 from travel.models import TravelPost
 from beauty.models import BeautyPost
+from itertools import chain
+from operator import attrgetter
 
 
 # Create your views here.
 def homepage(request):
-    """
-    :param request:
-    :return:
-    """
-    food_list = FoodPost.objects.published().order_by('-timestamp')[0:4]
-    for f in food_list:
-        print(f)
 
-    return render(request, 'homepage.html')
+    last_post, recent_posts = get_recent_posts()
+
+    context = {
+        'last_post': last_post,
+        'recent_posts': recent_posts,
+    }
+
+    return render(request, 'homepage.html', context)
 
 
 # def order_by_post_date
+
+def get_recent_posts():
+    """
+    Gets the recent posts
+    :param request:
+    :return:
+    """
+    food_posts = FoodPost.objects.published().order_by('-timestamp')
+    fitness_posts = FitnessPost.objects.published().order_by('-timestamp')
+    travel_posts = TravelPost.objects.published().order_by('-timestamp')
+    beauty_posts = BeautyPost.objects.published().order_by('-timestamp')
+
+    sorted_posts = sorted((chain(food_posts, fitness_posts, travel_posts, beauty_posts)),
+                          key=attrgetter('timestamp'), reverse=True)
+
+    return sorted_posts[0], sorted_posts[1:4]
+
+
 
