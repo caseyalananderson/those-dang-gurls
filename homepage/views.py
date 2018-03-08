d544467ab35b16b8503f883ef525d36563d2567e
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-07 23:10:33 -0700

    modified organization of the header

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from food.models import FoodPost
+from fitness.models import FitnessPost
+from travel.models import TravelPost
+from beauty.models import BeautyPost
 
-
-def homepage(request):
-    return render(request, 'homepage.html')
 
 # Create your views here.
+def homepage(request):
+    """
+    :param request:
+    :return:
+    """
+    food_list = FoodPost.objects.published().order_by('-timestamp')[0:4]
+    for f in food_list:
+        print(f)
+
+    return render(request, 'homepage.html')
+
+
+# def order_by_post_date
+
