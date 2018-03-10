8108dac3b90dd706c69d7d38cf6d20599a6c32c6
Casey Anderson <casey.alan.anderson@gmail.com>
2018-03-09 17:47:54 -0700

    added localsettings

---
+++ mysite/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thosedanggirls_db',
+        'USER': 'adanggirl',
+        'PASSWORD': 'password123454321',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
